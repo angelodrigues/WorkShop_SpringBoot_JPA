39a908cc51b062b6e8ef5f3542dd2d6ab060aad7
Angelo Barcelos <angelorodrigues318@gmail.com>
2023-11-12 13:26:05 -0300

    JPA repo, dependency injection, database seeding

---
+++ src/main/java/com/angelodrigues/course/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package com.angelodrigues.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.angelodrigues.course.entities.User;
+
+public interface UserRepository extends JpaRepository<User,Long>{
+    
+}
