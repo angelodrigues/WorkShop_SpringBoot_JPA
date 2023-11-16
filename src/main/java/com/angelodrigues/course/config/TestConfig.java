22d9e073fa77c5a22e0d2ca5cbe46f1d85727de5
Angelo Barcelos <angelorodrigues318@gmail.com>
2023-11-16 15:06:29 -0300

    Payment, one-to-one association

--- src/main/java/com/angelodrigues/course/config/TestConfig.java
+++ src/main/java/com/angelodrigues/course/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.angelodrigues.course.entities.Category;
 import com.angelodrigues.course.entities.Order;
 import com.angelodrigues.course.entities.OrderItem;
+import com.angelodrigues.course.entities.Payment;
 import com.angelodrigues.course.entities.Product;
 import com.angelodrigues.course.entities.User;
 import com.angelodrigues.course.entities.enums.OrderStatus;
@@ -80,5 +81,10 @@ public class TestConfig implements CommandLineRunner{
         OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 
         orderItemRepository.saveAll(Arrays.asList(oi1,oi2,oi3,oi4));
+
+        Payment pay1 = new Payment(null,Instant.parse("2019-06-20T21:53:07Z"), o1);
+        o1.setPayment(pay1);
+
+        orderRepository.save(o1);
     }
 }
