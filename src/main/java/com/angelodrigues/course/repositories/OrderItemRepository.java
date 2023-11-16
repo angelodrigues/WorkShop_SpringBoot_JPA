0ed8eaef3382a91eed8c4d536958aa412c7e0a1b
Angelo Barcelos <angelorodrigues318@gmail.com>
2023-11-16 14:41:52 -0300

    OrderItem, many-to-many part 2

---
+++ src/main/java/com/angelodrigues/course/repositories/OrderItemRepository.java
@@ -0,0 +1,9 @@
+package com.angelodrigues.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.angelodrigues.course.entities.OrderItem;
+
+public interface OrderItemRepository  extends JpaRepository<OrderItem,Long>{     
+    
+}
