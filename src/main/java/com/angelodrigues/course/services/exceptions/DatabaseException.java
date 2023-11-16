cc9296cb8bd7c31eb89048eaf52e8a56f8186329
Angelo Barcelos <angelorodrigues318@gmail.com>
2023-11-16 18:20:37 -0300

    Exception handling - delete

---
+++ src/main/java/com/angelodrigues/course/services/exceptions/DatabaseException.java
@@ -0,0 +1,9 @@
+package com.angelodrigues.course.services.exceptions;
+
+public class DatabaseException extends RuntimeException{
+    private static final long serialVersionUID = 1l;
+
+    public DatabaseException(String msg){
+        super(msg);
+    }
+}
